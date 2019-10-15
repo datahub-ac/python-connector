aeaf65bc6f9594f7eaf6a3b4f6772e36de6ccb89
Tamas Simon <tamas.simon@alphacruncher.com>
2019-10-15 16:45:59 +0200

    Added get_url auxiliary function

--- aclib/__init__.py
+++ aclib/__init__.py
@@ -6,7 +6,7 @@ from configparser import ConfigParser
 logger = logging.getLogger()
 
 
-def get_engine(db_name, schema_name):
+def get_url(db_name, schema_name):
     credential_filename = os.getenv("ACLIB_CREDENTIAL_FILENAME", os.path.expanduser("~") + "/.odbc.ini")
     credential_section = os.getenv("ACLIB_CREDENTIAL_SECTION", "datahub")
     # Create engine with credentials
@@ -16,14 +16,14 @@ def get_engine(db_name, schema_name):
     cred.read(credential_filename)
     credd = dict(cred.items(credential_section))
     snowflake_host = credd.get('host', "alphacruncher.eu-central-1")
-    config_dict = {}
-    config_dict['sqlalchemy.url'] = 'snowflake://' + credd['uid'] + ':' + credd['pwd'] + \
-                                    '@' + snowflake_host + '/?warehouse=' + credd['uid'] + \
-                                    '&database=' + db_name + '&schema=' + schema_name
-    config_dict['sqlalchemy.echo'] = False
-    logger.info('Connecting to ' + 'snowflake://' + credd['uid'] + ':*************' +
+    logger.info('Built SQLAlchemy URL: ' + 'snowflake://' + credd['uid'] + ':*************' +
                 '@' + snowflake_host + '/?warehouse=' + credd['uid'] +
                 '&database=' + db_name + '&schema=' + schema_name)
+    return 'snowflake://' + credd['uid'] + ':' + credd['pwd'] + \
+           '@' + snowflake_host + '/?warehouse=' + credd['uid'] + \
+           '&database=' + db_name + '&schema=' + schema_name
 
-    return engine_from_config(config_dict)
+
+def get_engine(db_name, schema_name):
+    return engine_from_config({'sqlalchemy.url': get_url(db_name, schema_name), 'sqlalchemy.echo': False})
 
