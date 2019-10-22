5fb9251479e0b8a3a4f61afbb4295e91c9a38f1d
Máté Kovács <mate.kovacs@alphacruncher.com>
2019-10-22 17:18:59 +0200

    dbpath integration

--- aclib/__init__.py
+++ aclib/__init__.py
@@ -2,6 +2,7 @@ import os
 import logging
 from sqlalchemy import engine_from_config
 from configparser import ConfigParser
+import re
 
 logger = logging.getLogger()
 
@@ -9,6 +10,8 @@ logger = logging.getLogger()
 def get_url(db_name=None, schema_name=None):
     credential_filename = os.getenv("ACLIB_CREDENTIAL_FILENAME", os.path.expanduser("~") + "/.odbc.ini")
     credential_section = os.getenv("ACLIB_CREDENTIAL_SECTION", "datahub")
+    
+    
     # Create engine with credentials
     cred = ConfigParser(interpolation=None)
     if not os.path.exists(credential_filename):
@@ -31,3 +34,22 @@ def get_url(db_name=None, schema_name=None):
 def get_engine(db_name, schema_name):
     return engine_from_config({'sqlalchemy.url': get_url(db_name, schema_name), 'sqlalchemy.echo': False})
 
+def get_dbpath():
+    path_filename = '/lifecycle/.dbpath'
+    path_file = open(path_filename, 'r')
+    first_line = path_file.readlines()[0]
+    # Remove end of line character
+    first_line = re.sub('`\n', '', first_line)
+    # Split at "." character
+    # This should have resulted in two substrings
+    split_arr = re.split("\".\"", first_line)
+
+    # Split removes the two quotes
+    split_arr[0] = split_arr[0] + "\""
+    split_arr[1] = "\"" + split_arr[1]
+
+    return split_arr[0], split_arr[1]
+
+def get_app_engine():
+    dbname, schemaname = get_dbpath()
+    return get_engine(db_name = dbname, schema_name = schemaname)
